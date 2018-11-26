ce4a28469edf1ecae5cd2e8486912f701c5603df
ekarp <emilyrkarp@gmail.com>
2018-11-26 14:04:21 -0500

    infinite scroll

--- site/xkcd/views.py
+++ site/xkcd/views.py
@@ -1,15 +1,27 @@
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.http import JsonResponse
 from xkcd.models import Comic
 
 
 def xkcds(request):
-    data = []
-    for comic in Comic.objects.order_by('-number'):
-        data.append({
+    stored_comics = Comic.objects.order_by('-number')
+    paginator = Paginator(stored_comics, 20)
+    page = request.GET.get('page', 1)
+
+    try:
+        data = paginator.page(page)
+    except PageNotAnInteger:
+        data = paginator.page(1)
+    except EmptyPage:
+        return JsonResponse({})
+
+    comics_json = []
+    for comic in data:
+        comics_json.append({
             'xkcd': comic.number,
             'date': comic.published,
             'name': comic.display_name,
             'file': comic.img_filename,
         })
 
-    return JsonResponse(data, safe=False)
+    return JsonResponse({'comics': comics_json, 'page': page}, safe=False)
