3cf278410229de7ae2bf077e15e9f0e7b737993c
ekarp <emilyrkarp@gmail.com>
2019-01-15 13:32:45 -0500

    check if comics are up to date

--- site/xkcd/management/commands/scrape_for_new_comics.py
+++ site/xkcd/management/commands/scrape_for_new_comics.py
@@ -12,15 +12,18 @@ class Command(BaseCommand):
         comics = []
         counter = 0
 
-        stored_comics = Comic.objects.all().order_by('published')
-        latest_number = stored_comics[0] if stored_comics else 0
+        stored_comics = Comic.objects.all().order_by('-published')
+        latest_number = stored_comics[0].number if stored_comics else 0
 
         base_link = 'https://xkcd.com/'
         res = requests.get(base_link)
-        next_number = ''
 
-        while latest_number is not next_number and counter < 5:
-            res = requests.get(f'{base_link}/{next_number}')
+        # Get the comic number
+        page_soup = BeautifulSoup(res.text, features="html.parser")
+        comic_number = re.search(r'(?<=https://xkcd.com/)\d+', page_soup.text).group(0)
+
+        while str(latest_number) != comic_number and counter < 5:
+            res = requests.get(f'{base_link}/{comic_number}')
             res.raise_for_status()
 
             # Parse the page and find the comic image element
@@ -30,7 +33,6 @@ class Command(BaseCommand):
 
             # Save the comic image to the disk
             try:
-                img_res = requests.get(f'https://imgs.xkcd.com/comics/{basename}')
                 img_res = requests.get(f'https://imgs.xkcd.com/comics/{basename}')
                 img_res.raise_for_status()
                 print(f'Downloading {basename}')
@@ -44,16 +46,12 @@ class Command(BaseCommand):
 
             except requests.exceptions.HTTPError:
                 print(f'Could not find xkcd {number}')
-                next_number -= 1
+                comic_number -= 1
                 continue
-
-            if not next_number:
-                m = re.search(r'(?<=https://xkcd.com/)\d+', page_soup.text)
-                next_number = m.group(0)
 
             # Save the comic data to the array to be persisted
             comics.append(Comic(
-                number = int(next_number),
+                number = int(comic_number),
                 img_filename = basename,
                 display_name = img_elem.get('alt'),
                 description = img_elem.get('title'),
@@ -61,7 +59,7 @@ class Command(BaseCommand):
 
             # Find the link to the next comic page
             prev_link = page_soup.select('a[rel="prev"]')[0]
-            next_number = prev_link.get('href').replace('/', '')
+            comic_number = prev_link.get('href').replace('/', '')
             counter += 1
 
         # Find the published date for each downloaded comic on the archives page
@@ -76,7 +74,8 @@ class Command(BaseCommand):
             # Save the date from the archive link to the comic object
             for i in range(len(comics)):
                 comics[i].published = link_list[i].get('title')
-                print(comics[i].published)
 
-        print('Saving downloaded comics')
-        Comic.objects.bulk_create(comics)
+            print('Saving downloaded comics')
+            Comic.objects.bulk_create(comics)
+
+        print('Comics are up to date.')
